a3eaec2121658ffe140fd85ccc9f6fdd82b0f5fa
Jon Lester <lesterjt@gmail.com>
2020-01-29 16:36:49 -0500

    Refactoring
    removed dependency on Microsoft.Identity.Web
    
    changed default flow from implicit to auth code

--- OCP.Msal.Proxy.Web/Startup.cs
+++ OCP.Msal.Proxy.Web/Startup.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.AzureAD.UI;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
@@ -12,8 +16,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Identity.Web;
 using Microsoft.IdentityModel.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace OCP.Msal.Proxy.Web
 {
@@ -25,37 +29,39 @@ namespace OCP.Msal.Proxy.Web
         }
 
         public IConfiguration Configuration { get; }
+        public const string AzureAdConfigSection = "AzureAd";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<AzureADOptions>(Configuration.GetSection("AzureAd").Get<AzureADOptions>());
+
+            AzureADOptions azureAdOptions = Configuration.GetSection(AzureAdConfigSection).Get<AzureADOptions>();
+            services.AddSingleton<AzureADOptions>(azureAdOptions);
             services.Configure<CookiePolicyOptions>(options => { options.MinimumSameSitePolicy = SameSiteMode.None; });
 
-            // allow Bearer tokens for non-interactive callers
-            services.AddProtectedWebApi(Configuration);
-            // Sign-in users with the Microsoft identity platform
-            services.AddMicrosoftIdentityPlatformAuthentication(Configuration);
+            //services.AddHttpContextAccessor();
 
-            services.AddDataProtection()
-                .PersistKeysToFileSystem(new DirectoryInfo(Configuration["DataProtectionFileLocation"]));
+            
+            services.AddAuthentication()
+                // allow Bearer tokens for non-interactive applications
+                .AddAzureADBearer(options => Configuration.Bind(AzureAdConfigSection, options))
+                // web applications
+                .AddAzureAD(options => Configuration.Bind(AzureAdConfigSection, options));
 
-            services.AddMvc(options =>
-                {
-                    var policy = new AuthorizationPolicyBuilder()
-                        .RequireAuthenticatedUser()
-                        .Build();
-                    options.Filters.Add(new AuthorizeFilter(policy));
-                })
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.Configure<ForwardedHeadersOptions>(options =>
+            //configure bearer options
+            services.Configure<JwtBearerOptions>(AzureADDefaults.JwtBearerAuthenticationScheme, (configureOptions) =>
             {
-                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-                options.ForwardLimit = 2;
-                options.KnownNetworks.Clear();
-                options.KnownProxies.Clear();
+                configureOptions.Authority += "/v2.0";
             });
 
+            //configure OIDC options
+            services.Configure<OpenIdConnectOptions>(AzureADDefaults.OpenIdScheme, (configureOptions) =>
+            {
+                configureOptions.Authority += "/v2.0";
+                configureOptions.ResponseType = "code";
+            });
+
+            //add authz policies to distinguish between application types
             services.AddAuthorization(options =>
             {
                 options.AddPolicy("api", policy =>
@@ -69,6 +75,24 @@ namespace OCP.Msal.Proxy.Web
                     policy.RequireAuthenticatedUser();
                 });
             });
+
+            //required to ensure consistency across multiple nodes
+            services.AddDataProtection()
+                .PersistKeysToFileSystem(new DirectoryInfo(Configuration["DataProtectionFileLocation"]));
+
+            
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+                options.ForwardLimit = 2;
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
+
+
 
         }
 
@@ -103,19 +127,13 @@ namespace OCP.Msal.Proxy.Web
                 });
             }
 
-            //app.UseHttpsRedirection();
-            //app.UseStaticFiles();
-
             app.UseCookiePolicy();
             app.UseAuthentication();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    "default",
-                    "{controller=Msal}/{action=Index}/{id?}");
+            app.UseMvc();
 
-            });
         }
+
+
     }
 }
