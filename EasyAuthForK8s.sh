5c3990039f0efc1b7f80d597a3b5a6ba1aa9b162
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2020-01-29 10:51:29 -0600

    Updated az ad app registration

--- EasyAuthForK8s.sh
+++ EasyAuthForK8s.sh
@@ -71,7 +71,21 @@ echo "COMPLETE @ $(date +"%T"): Deployed sample app"
 
 echo "BEGIN @ $(date +"%T"): Register AAD Application..."
 
-CLIENT_ID=$(az ad app create --display-name $AD_APP_NAME --homepage $HOMEPAGE --reply-urls $REPLY_URLS -o json | jq -r '.appId')
+cat << EOF > manifest.json
+[
+   {
+      "resourceAccess" : [
+         {
+            "id" : "e1fe6dd8-ba31-4d61-89e7-88639da4683d",
+            "type" : "Scope"
+         }
+      ],
+      "resourceAppId" : "00000003-0000-0000-c000-000000000000"
+   }
+]
+EOF
+
+CLIENT_ID=$(az ad app create --display-name $AD_APP_NAME --homepage $HOMEPAGE --reply-urls $REPLY_URLS --required-resource-accesses @manifest.json -o json | jq -r '.appId')
 echo "CLIENT_ID: " $CLIENT_ID
 
 OBJECT_ID=$(az ad app show --id $CLIENT_ID -o json | jq '.objectId' -r)
