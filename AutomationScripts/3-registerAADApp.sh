046d81a7f08fe208931e94941d8f75ee045574e9
Jon Lester <lesterjt@gmail.com>
2021-10-07 15:59:35 -0400

    Update 3-registerAADApp.sh
    add polling after object creation to avoid intermittent errors with the script running too fast.  Removed unused template fragment.

--- AutomationScripts/3-registerAADApp.sh
+++ AutomationScripts/3-registerAADApp.sh
@@ -1,25 +1,24 @@
 #!/bin/sh -x
 
 echo "BEGIN @ $(date +"%T"): Register AAD Application..."
-cat << EOF > manifest.json
-[
-   {
-      "resourceAccess" : [
-         {
-            "id" : "e1fe6dd8-ba31-4d61-89e7-88639da4683d",
-            "type" : "Scope"
-         }
-      ],
-      "resourceAppId" : "00000003-0000-0000-c000-000000000000"
-   }
-]
-EOF
 
 CLIENT_ID=$(az ad app create --display-name $AD_APP_NAME --homepage $HOMEPAGE --reply-urls $REPLY_URLS --required-resource-accesses @manifest.json -o json | jq -r '.appId')
 echo "CLIENT_ID: " $CLIENT_ID
 
-OBJECT_ID=$(az ad app show --id $CLIENT_ID -o json | jq '.objectId' -r)
+# AAD core store is eventually consistent.  Usually we can retrieve the object on the first try after creation,
+# but sometimes it takes a few seconds.
+while test -z "$OBJECT_ID"
+do
+  sleep 3
+  echo "Polling status of AAD object creation for app...."
+  OBJECT_ID=$(az ad app show --id $CLIENT_ID -o json | jq '.objectId' -r)
+done
+
 echo "OBJECT_ID: " $OBJECT_ID
+
+# the default manifest template in the CLI command adds a permission we do not need
+# Here we disable it, then delete it.  This also keeps us from getting dealing with
+# unnessary consent prompts
 az ad app update --id $OBJECT_ID --set oauth2Permissions[0].isEnabled=false
 az ad app update --id $OBJECT_ID --set oauth2Permissions=[]
 
