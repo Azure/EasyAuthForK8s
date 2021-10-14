3ba8a0b5d847bb06b0cea720f507d925e7352742
Danielkon96 <danielkon96@yahoo.com>
2021-10-14 22:09:01 +0000

    add variables to test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,65 @@
 #!/bin/sh -x
 
+# Initialize Variables for flags
+A=''
+C=''
+R=''
+E=''
+D=''
+L=''
+I=''
+P=''
+
+while getopts "a:c:r:e:d:l:i:n:s:p:h" OPTION
+do
+	case $OPTION in
+		a)
+			# echo "The value of -a is ${OPTARG} - AD_APP_NAME"
+            A=$OPTARG ;;
+	    c)
+			# echo "The value of -c is ${OPTARG} - CLUSTER_NAME"
+            C=$OPTARG ;;
+        r)
+			# echo "The value of -r is ${OPTARG} - CLUSTER_RG"
+            R=$OPTARG ;;
+        e)
+			# echo "The value of -e is ${OPTARG} - EMAIL"
+            E=$OPTARG ;;
+        d)
+			# echo "The value of -d is ${OPTARG} - EMAIL_DOMAIN"
+            D=$OPTARG ;;
+        l)
+			# echo "The value of -l is ${OPTARG} - LOCATION"
+            L=$OPTARG ;;
+        i)
+			# echo "The value of -i is ${OPTARG} - INPUTIMAGE"
+            I=$OPTARG ;;
+        p) 
+            # echo "The value of -p is ${OPTARG} - SKIP_CLUSTER_CREATION"
+            P=$OPTARG ;;
+		h)
+            # Change to how others show it like az
+            echo "HELP: Here are the flags and their variables"
+			echo "REQUIRED: -a is for AD_APP_NAME"
+            echo "REQUIRED: -c is for CLUSTER_NAME *Note: Cluster Name must be unique*" 
+            echo "REQUIRED: -r is for CLUSTER_RG"
+            echo "REQUIRED: -e is for EMAIL"
+            echo "REQUIRED: -d is for EMAIL_DOMAIN"
+            echo "REQUIRED: -l is for LOCATION"
+            echo "OPTOINAL: -i is for INPUTIMAGE"
+            echo "OPTOINAL: -p is for SKIP_CLUSTER_CREATION"
+			exit ;;
+	esac
+done
+
 echo ""
 echo "BEGIN @ $(date +"%T"): START OF END-TO-END TEST"
-. ./main.sh -a dk-msal-test51 -c dk-msal-test51 -r dk-msal-test51 -e dakondra@microsoft.com -d microsoft.com -l eastus
+bash ./main.sh -a $A -c $C -r $R -e $E -d $D -l $L
 
-echo "Grabbed homepage: " $HOMEPAGE
-RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" $HOMEPAGE)
+APP_NAME="$A.$L.cloudapp.azure.com"
+WEBPAGE=https://$APP_NAME
+echo "Grabbed homepage: " $WEBPAGE
+RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" $WEBPAGE)
 echo "response code: " $RESPONSE_CODE
 
 if [ "$RESPONSE_CODE" != "302" ]; then
